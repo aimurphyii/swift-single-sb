92ad41364c42be039324d534309614263e20d3b0
Aileen Murphy <ai.murf2@gmail.com>
2019-11-01 23:39:29 -0700

    making a settings page in swift

---
+++ swft-single-sb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swft-single-sb
+//
+//  Created by Aileen Murphy on 11/1/19.
+//  Copyright © 2019 Aileen Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
